47ded7bb1cda5e706215a6b030ee8e98bf35eafc
Iliya Nanov <nanov.iliya@gmail.com>
2025-07-17 10:42:18 +0200

    add mplfinance

--- experiments/bayesian_gmm_mcmc/functions.py
+++ experiments/bayesian_gmm_mcmc/functions.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import norm, beta, halfnorm
 from sklearn.mixture import GaussianMixture
-
+import mplfinance as mpf
 
 def bayesian_gmm_mcmc(
     data,
